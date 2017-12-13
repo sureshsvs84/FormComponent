3562afcc853766c66a5432d6a319a4970c469a39
sureshsvs84 <suresh.s@smnetserv.com>
2017-12-14 00:31:21 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React , { Component } from 'react';
+import FormContainer from '../container/formContainer'
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="columns">
+          <div className="col-md-9 centered">
+            <h3>React.js Controlled Form Components</h3>
+            <FormContainer />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
